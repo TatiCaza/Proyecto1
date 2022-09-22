60846bea84f2440f769403e848d88a4511fc63cd
TatiCazazola <tati_anabel96@outlook.com>
2022-09-22 07:36:29 +0200

    Inilizacion web Django

---
+++ Proyecto1/view.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader
+from AppCoder.models import Curso
+
+def home (request, name):
+    return HttpResponse (f'Hola soy {name}')
+
+def homePage (self):
+    lista = [1,2,3,4,5,6,7,8,9]
+    data = {'nombre': 'Tatiana', 'apellido':'Cazazola', 'lista': lista}
+    planilla = loader.get_template('home.html')
+    documento = planilla.render(data)
+    return HttpResponse (documento)
+
+def curso (self):
+    #planilla = loader.get_template('cursos.html')
+    curso = Curso(nombre="UX/UI", camada="12345")
+    curso.save()
+    documento = f'Curso: {curso.nombre} camada: {curso.camada}'
+    return HttpResponse (documento)
